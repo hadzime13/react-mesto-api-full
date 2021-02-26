76a6be6beeeab463a2ca38c2a33f0dbb14b2c11d
Andrey Varlamov <hadzime@yandex.ru>
2021-02-26 23:53:09 +0300

    edited baseUrl

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -113,7 +113,7 @@ class Api {
 
 
 const api = new Api({
-  url: 'http://localhost:3000',
+  url: 'api.hadzime.students.nomoredomains.icu',
   headers: {
     'Content-Type': 'application/json'
   }
