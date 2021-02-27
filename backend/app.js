abb20bffb102e43a51e4114d1601c3c60d62b53f
Andrey Varlamov <hadzime@yandex.ru>
2021-02-27 03:05:53 +0300

    panic

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -32,7 +31,7 @@ app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
-}); 
+});
 
 app.use('/signup', registerRouter);
 app.use('/signin', loginRouter);
